3cf6c0fa41d15d059a726c88fedf8d999ca5513b
James Sullivan <james.sullivan@live.co.uk>
2019-02-01 14:36:47 +0000

    adding bootstrap and working on nav

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+console.log('test');
+// $(document).ready(function () {
+    $(".btn-primary:first").click(function () {
+        // $(this).button('toggle');
+        $("p").toggleClass("tester-color");
+    });
+// });
+
+// $("p").css("color","blue");
