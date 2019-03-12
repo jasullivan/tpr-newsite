da8ca8fee5b8f38730d062b684cbc59e11fded8b
James Sullivan <james.sullivan@live.co.uk>
2019-03-12 12:02:19 +0000

    testing lazysizes on projects

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,13 @@
+var lazy = function lazy() {
+    document.addEventListener('lazyloaded', function (e) {
+        e.target.parentNode.classList.add('image-loaded');
+        e.target.parentNode.classList.remove('loading');
+    });
+}
+
+lazy();
+
+
 /*******************************
 // slider lazy loading
 ********************************/
