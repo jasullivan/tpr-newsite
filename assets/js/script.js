10e18fb6b950ca882f52ae180c3a1fde51fba2e6
James Sullivan <james.sullivan@live.co.uk>
2019-04-09 19:43:49 +0100

    updating html

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,4 @@
-/******************************* 
-// ARCHIVE DROPDOWN
-********************************/
-const archive = document.querySelector('.archive-button');
-const projects = document.querySelector(".archive-projects");
-archive.addEventListener('click', function () {
-    if (projects.style.maxHeight) {
-        projects.style.maxHeight = null;
-        archive.classList.remove("archive-active");
-    } else {
-        projects.style.maxHeight = projects.scrollHeight + 'px';
-        console.log(projects.style.maxHeight)
-        archive.classList.add("archive-active");
-    }
-})
-/******************************* 
-// ARCHIVE DROPDOWN ENDS
-********************************/
+
 
 
 // mob nav button
