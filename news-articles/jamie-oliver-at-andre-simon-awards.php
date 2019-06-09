b0c568d780fb8b4aee08a3cb4781258343a6024d
James Sullivan <james.sullivan@live.co.uk>
2019-06-09 12:34:39 +0100

    converting news articles to php

--- news-articles/jamie-oliver-at-andre-simon-awards.php
+++ news-articles/jamie-oliver-at-andre-simon-awards.php
@@ -1,65 +1,32 @@
+<?php
+	// CHANGE THE $NAV_CLASS VARIABLE TO HOME OR ABOUT AND SEE THAT IT CHANGES WHICH LI IS ACTIVE
+	$nav_class = 'nav-news';
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="utf-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge"><link rel="shortcut icon" type="image/png" href="../../assets/images/tpr-favicon.png">
-    <title>tpr media &#124; News</title>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge"><link rel="shortcut icon" type="image/png" href="../assets/images/tpr-favicon.png">
+    <title>tpr media | News</title>
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS"
         crossorigin="anonymous">
-    <link rel="stylesheet" type="text/css" media="screen" href="../../assets/styles/css/style.css" />
+    <link rel="stylesheet" type="text/css" media="screen" href="../assets/styles/css/style.css" />
     <script>
         // Picture element HTML5 shiv
         document.createElement("picture");
     </script>
-    <script src="../../assets/js/picturefill.min.js" async></script>
-    <script src="../../assets/js/lazysizes.min.js"></script>
+    <script src="../assets/js/picturefill.min.js" async></script>
+    <script src="../assets/js/lazysizes.min.js"></script>
 </head>
 
 <body>
     <!-- navbar -->
-    <nav role="navigation" aria-label="Main navigation" class="navbar navbar-expand-lg navbar-light bg-white p-4 tpr-nav">
-        <a class="navbar-brand" href="../../index.html">
-            <img src="../../assets/images/logos/logo.png" alt="tpr media logo">
-        </a>
-        <div class="tpr-nav__social-icons pt-5">
-            <span class="icon icon__twitter icon-issuu" aria-hidden="true"></span>
-            <span class="icon icon__twitter icon-Twitter-black" aria-hidden="true"></span>
-            <span class="icon icon__linkedin icon-LinkedIn-black" aria-hidden="true"></span>
-        </div>
-        <button class="navbar-toggler mt-5 mr-3" type="button" data-toggle="collapse" data-target="#navbarText"
-            aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="icon icon__hamburger icon-hamburger show" aria-hidden="true"></span>
-            <span class="icon icon__cross icon-cross hide" aria-hidden="true"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarText">
-            <hr>
-            <ul class="navbar-nav mr-auto pt-lg-5">
-                <li class="nav-item text-center">
-                    <a class="pr-lg-5 pl-lg-5 tpr-nav__link" href="../../about.html">About</a>
-                </li>
-                <li class="nav-item text-center">
-                    <a class="pr-lg-5 tpr-nav__link" href="../../projects.html">Projects</a>
-                </li>
-                <li class="nav-item text-center">
-                    <a class="pr-lg-5 tpr-nav__link" href="../../clients.html">Clients</a>
-                </li>
-                <li class="nav-item text-center">
-                    <a class="pr-lg-5 tpr-nav__link" href="../../approach.html">Approach</a>
-                </li>
-                <li class="nav-item text-center">
-                    <a class="pr-lg-5 tpr-nav__link active" href="../../news.html">News</a>
-                </li>
-                <li class="nav-item text-center">
-                    <a class="tpr-nav__link" href="../../contact.html">Contact</a>
-                </li>
-            </ul>
-        </div>
-    </nav>
+    <?php include '../sub-navigation.php'; ?>
     <!-- navbar -->
 
-    <article>
+    <article role="article">
         <!-- blue header bar -->
         <header class="container-fluid pt-4">
             <div class="tpr-blue-bar">
@@ -77,9 +44,9 @@
                         
                         <img 
                             data-sizes="auto" 
-                            data-src="../../assets/images/news/andre-simon-2018-news-2.jpg" 
-                            src="../../assets/images/news/andre-simon-2018-news-2-preview.jpg" 
-                            data-srcset="../../assets/images/news/andre-simon-2018-news-2_400w.jpg 400w, ../../assets/images/news/andre-simon-2018-news-2.jpg 800w" 
+                            data-src="../assets/images/news/andre-simon-2018-news-2.jpg" 
+                            src="../assets/images/news/andre-simon-2018-news-2-preview.jpg" 
+                            data-srcset="../assets/images/news/andre-simon-2018-news-2_400w.jpg 400w, ../assets/images/news/andre-simon-2018-news-2.jpg 800w" 
                             alt="Jamie Oliver at Andre Simon Awards" 
                             class="lazyload tpr-article-content__image">
                         
@@ -107,8 +74,8 @@
                                 href="https://www.jamieoliver.com/the-fifteen-apprentice-programme/home" target="_blank" rel="nofollow noopener">Fifteen
                                 restaurant programme</a>.</p>
                                 
-                       <img data-sizes="auto" data-src="../../assets/images/news/andre-simon-2018-news-3.jpg" src="../../assets/images/news/andre-simon-2018-news-3-preview.jpg"
-                            data-srcset="../../assets/images/news/andre-simon-2018-news-3_400w.jpg 400w, ../../assets/images/news/andre-simon-2018-news-3.jpg 800w"
+                       <img data-sizes="auto" data-src="../assets/images/news/andre-simon-2018-news-3.jpg" src="../assets/images/news/andre-simon-2018-news-3-preview.jpg"
+                            data-srcset="../assets/images/news/andre-simon-2018-news-3_400w.jpg 400w, ../assets/images/news/andre-simon-2018-news-3.jpg 800w"
                             alt="Jamie Oliver at Andre Simon Awards" class="lazyload tpr-article-content__image">
                         
                         <p class="tpr-article-content__paragraph">Blogger and author <a class="tpr-article-content__link" href="https://twitter.com/racheleats"
@@ -124,8 +91,8 @@
                             yourself, you might like to check out <a class="tpr-article-content__link" href="http://www.theguardian.com/lifeandstyle/2016/feb/02/ragu-alla-bolognese-recipe-rachel-roddy-kitchen-sink-tales"
                                 target="_blank" rel="nofollow noopener">this recent feature in the Guardian</a>.</p>
 
-                        <img data-sizes="auto" data-src="../../assets/images/news/andre-simon-2018-news-4.jpg" src="../../assets/images/news/andre-simon-2018-news-4-preview.jpg"
-                            data-srcset="../../assets/images/news/andre-simon-2018-news-4_400w.jpg 400w, ../../assets/images/news/andre-simon-2018-news-4.jpg 800w"
+                        <img data-sizes="auto" data-src="../assets/images/news/andre-simon-2018-news-4.jpg" src="../assets/images/news/andre-simon-2018-news-4-preview.jpg"
+                            data-srcset="../assets/images/news/andre-simon-2018-news-4_400w.jpg 400w, ../assets/images/news/andre-simon-2018-news-4.jpg 800w"
                             alt="Rachel Roddy at Andre Simon Awards" class="lazyload tpr-article-content__image">
                         
                         <p class="tpr-article-content__paragraph">Meanwhile, investigative journalist <a class="tpr-article-content__link"
@@ -139,8 +106,8 @@
                             Thirsty Dragon on <a class="tpr-article-content__link" href="http://www.bbc.co.uk/programmes/b006qtl3" target="_blank"
                                 rel="nofollow noopener">BBC Radio 4's The World Tonight</a> this Friday.</p>
                             
-                        <img data-sizes="auto" data-src="../../assets/images/news/andre-simon-2018-news-5.jpg" src="../../assets/images/news/andre-simon-2018-news-5-preview.jpg"
-                            data-srcset="../../assets/images/news/andre-simon-2018-news-5_400w.jpg 400w, ../../assets/images/news/andre-simon-2018-news-5.jpg 800w"
+                        <img data-sizes="auto" data-src="../assets/images/news/andre-simon-2018-news-5.jpg" src="../assets/images/news/andre-simon-2018-news-5-preview.jpg"
+                            data-srcset="../assets/images/news/andre-simon-2018-news-5_400w.jpg 400w, ../assets/images/news/andre-simon-2018-news-5.jpg 800w"
                             alt="Suzanne Mustacich at Andre Simon Awards" class="lazyload tpr-article-content__image">
                         
                         <p class="tpr-article-content__paragraph"><a class="tpr-article-content__link" href="https://www.harpercollins.co.uk/9780007549702/first-bite"
@@ -152,8 +119,8 @@
                             from food psychologists, neuroscientists and nutritionists to reveal how our food habits are shaped by family,
                             culture, memory, gender, hunger and love.</p>
 
-                        <img data-sizes="auto" data-src="../../assets/images/news/andre-simon-2018-news-6.jpg" src="../../assets/images/news/andre-simon-2018-news-6-preview.jpg"
-                            data-srcset="../../assets/images/news/andre-simon-2018-news-6_400w.jpg 400w, ../../assets/images/news/andre-simon-2018-news-6.jpg 800w"
+                        <img data-sizes="auto" data-src="../assets/images/news/andre-simon-2018-news-6.jpg" src="../assets/images/news/andre-simon-2018-news-6-preview.jpg"
+                            data-srcset="../assets/images/news/andre-simon-2018-news-6_400w.jpg 400w, ../assets/images/news/andre-simon-2018-news-6.jpg 800w"
                             alt="Bee Wilson at Andre Simon Awards" class="lazyload tpr-article-content__image">
                         
                         <p class="tpr-article-content__paragraph">This year's shortlisted authors – selected from over 150 entries – also
@@ -190,23 +157,7 @@
     <!-- article content -->
 
     <!-- footer -->
-    <footer class="container-fluid px-0">
-        <div class="row bg-dark m-0">
-            <div class="col-12">
-                <div class="tpr-footer p-5 text-center">
-                    <div class="tpr-footer__content">
-                        <img src="../../assets/images/logos/logo_white.png" alt="" class="tpr-footer__logo-main">
-                        <p class="text-center px-5 pt-5">Highgate, London</p>
-                        <a href="tel:4402083477020" class="text-center tpr-footer__tel-link">+44 0208 347 7020</a>
-                        <h4 class="pt-5">tpr media consultants</h4>
-                    </div>
-                    <div class="tpr-footer__content--left">
-                        <img src="../../assets/images/logos/cipr_small.jpg" alt="" class="tpr-footer__content--left">
-                    </div>
-                </div>
-            </div>
-        </div>
-    </footer>
+    <?php include '../sub-footer.php'; ?>
     <!-- footer -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
@@ -215,7 +166,7 @@
         crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k"
         crossorigin="anonymous"></script>
-    <script src="../../assets/js/script.js"></script>
+    <script src="../assets/js/script.js"></script>
 
 </body>
 
--- news-articles/vr-is-for-everyone.php
+++ news-articles/vr-is-for-everyone.php
@@ -1,65 +1,32 @@
+<?php
+	// CHANGE THE $NAV_CLASS VARIABLE TO HOME OR ABOUT AND SEE THAT IT CHANGES WHICH LI IS ACTIVE
+	$nav_class = 'nav-news';
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="utf-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge"><link rel="shortcut icon" type="image/png" href="../../assets/images/tpr-favicon.png">
-    <title>tpr media &#124; News</title>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge"><link rel="shortcut icon" type="image/png" href="../assets/images/tpr-favicon.png">
+    <title>tpr media | News</title>
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS"
         crossorigin="anonymous">
-    <link rel="stylesheet" type="text/css" media="screen" href="../../assets/styles/css/style.css" />
+    <link rel="stylesheet" type="text/css" media="screen" href="../assets/styles/css/style.css" />
     <script>
         // Picture element HTML5 shiv
         document.createElement("picture");
     </script>
-    <script src="../../assets/js/picturefill.min.js" async></script>
-    <script src="../../assets/js/lazysizes.min.js"></script>
+    <script src="../assets/js/picturefill.min.js" async></script>
+    <script src="../assets/js/lazysizes.min.js"></script>
 </head>
 
 <body>
     <!-- navbar -->
-    <nav role="navigation" aria-label="Main navigation" class="navbar navbar-expand-lg navbar-light bg-white p-4 tpr-nav">
-        <a class="navbar-brand" href="../../index.html">
-            <img src="../../assets/images/logos/logo.png" alt="tpr media logo">
-        </a>
-        <div class="tpr-nav__social-icons pt-5">
-            <span class="icon icon__twitter icon-issuu" aria-hidden="true"></span>
-            <span class="icon icon__twitter icon-Twitter-black" aria-hidden="true"></span>
-            <span class="icon icon__linkedin icon-LinkedIn-black" aria-hidden="true"></span>
-        </div>
-        <button class="navbar-toggler mt-5 mr-3" type="button" data-toggle="collapse" data-target="#navbarText"
-            aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="icon icon__hamburger icon-hamburger show" aria-hidden="true"></span>
-            <span class="icon icon__cross icon-cross hide" aria-hidden="true"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarText">
-            <hr>
-            <ul class="navbar-nav mr-auto pt-lg-5">
-                <li class="nav-item text-center">
-                    <a class="pr-lg-5 pl-lg-5 tpr-nav__link" href="../../about.html">About</a>
-                </li>
-                <li class="nav-item text-center">
-                    <a class="pr-lg-5 tpr-nav__link" href="../../projects.html">Projects</a>
-                </li>
-                <li class="nav-item text-center">
-                    <a class="pr-lg-5 tpr-nav__link" href="../../clients.html">Clients</a>
-                </li>
-                <li class="nav-item text-center">
-                    <a class="pr-lg-5 tpr-nav__link" href="../../approach.html">Approach</a>
-                </li>
-                <li class="nav-item text-center">
-                    <a class="pr-lg-5 tpr-nav__link active" href="../../news.html">News</a>
-                </li>
-                <li class="nav-item text-center">
-                    <a class="tpr-nav__link" href="../../contact.html">Contact</a>
-                </li>
-            </ul>
-        </div>
-    </nav>
+    <?php include '../sub-navigation.php'; ?>
     <!-- navbar -->
 
-    <article>
+    <article role="article">
         <!-- blue header bar -->
         <header class="container-fluid pt-4">
             <div class="tpr-blue-bar">
@@ -78,11 +45,20 @@
 
                         <img 
                             data-sizes="auto" 
-                            data-src="../../assets/images/news/catherine-allen-news.jpg" 
-                            src="../../assets/images/news/catherine-allen-news-preview.jpg"
-                            data-srcset="../../assets/images/news/catherine-allen-news_400w.jpg 400w, ../../assets/images/news/catherine-allen-news.jpg 800w"
+                            data-src="../assets/images/news/catherine-allen-news.jpg" 
+                            src="../assets/images/news/catherine-allen-news-preview.jpg"
+                            data-srcset="../assets/images/news/catherine-allen-news_400w.jpg 400w, ../assets/images/news/catherine-allen-news.jpg 800w"
                             alt="Catherine Allen CEO Limina Immersive" 
                             class="lazyload tpr-article-content__image">
+                        <!-- <img 
+                            data-sizes="auto"
+                            data-srcset="
+                                ../assets/images/projects/accidental-anarchist-med.jpg 800w, 
+                                ../assets/images/projects/accidental-anarchist-lrg.jpg"
+                            data-src="../assets/images/projects/accidental-anarchist-med.jpg"
+                            src="../assets/images/projects/accidental-anarchist-preview.jpg" 
+                            alt="Presenter Carne Ross standing in front of an anarchist mural" class="lazyload tpr-article-content__image"
+                        > -->
                     
                         <p class="tpr-article-content__paragraph">Catherine's company, Limina Immersive, curates VR-only festivals at arts centres and cinemas throughout the UK, introducing the wider public to VR and unlocking this new medium's creative possibilities.</p>
                         <p class="tpr-article-content__paragraph">Highlights include I Saw The Future (Arte, François Vautier) in which Arthur C. Clarke, the co-writer of Stanley Kubrick's 2001: A Space Odyssey, reveals his vision of the future (it's scary how much he got right!) and Grenfell: Our Home (Parable, 59 Productions, Channel 4), an intimate portrait through the voices of its residents.</p>
@@ -102,23 +78,7 @@
     </article>
 
     <!-- footer -->
-    <footer class="container-fluid px-0">
-        <div class="row bg-dark m-0">
-            <div class="col-12">
-                <div class="tpr-footer p-5 text-center">
-                    <div class="tpr-footer__content">
-                        <img src="../../assets/images/logos/logo_white.png" alt="" class="tpr-footer__logo-main">
-                        <p class="text-center px-5 pt-5">Highgate, London</p>
-                        <a href="tel:4402083477020" class="text-center tpr-footer__tel-link">+44 0208 347 7020</a>
-                        <h4 class="pt-5">tpr media consultants</h4>
-                    </div>
-                    <div class="tpr-footer__content--left">
-                        <img src="../../assets/images/logos/cipr_small.jpg" alt="" class="tpr-footer__content--left">
-                    </div>
-                </div>
-            </div>
-        </div>
-    </footer>
+    <?php include '../sub-footer.php'; ?>
     <!-- footer -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
@@ -127,7 +87,7 @@
         crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k"
         crossorigin="anonymous"></script>
-    <script src="../../assets/js/script.js"></script>
+    <script src="../assets/js/script.js"></script>
 
 </body>
 
